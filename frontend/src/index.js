2ec7c6554d1c64d428b3c4c4ee62adf24143270a
HienNTM <hienntm134@gmail.com>
2017-12-31 22:59:18 +0700

    remove an unneeded file

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,8 +5,6 @@ import { createStore, applyMiddleware, compose } from 'redux'
 import { Provider } from 'react-redux'
 import promise from 'redux-promise'
 import thunk from 'redux-thunk'
-
-//import App from './components/App'
 import './index.css'
 import registerServiceWorker from './registerServiceWorker'
 import rootReducer from './reducers'
@@ -16,19 +14,7 @@ import Header from './components/Header'
 import PostsList from './components/PostsList'
 import DetailedPost from './components/DetailedPost'
 import PostCreateEdit from './components/PostCreateEdit'
-import NoMatch from './components/NoMatch'
 import './App.css'
-
-//const logger = store => next => action => {
-//	console.group(action.type)
-//	console.info('dispatching', action)
-//	let result = next(action)
-//	console.log('next state', store.getState())
-//	console.groupEnd(action.type)
-//	return result
-//}
-
-//window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 
@@ -45,16 +31,11 @@ ReactDOM.render(
 		  <div className="App">
 			<Header />
 			<Switch>
-			
 				<Route path="/posts/create-edit" component={PostCreateEdit}/>
 				<Route path="/posts/:id/edit" component={PostCreateEdit}/>
 				<Route path="/:category/:id" component={DetailedPost}/>
-				<Route path="/:category" component={PostsList}/>
+				<Route path="/:category" component={PostsList}/>				
 				<Route exact path="/" component={PostsList}/>
-				<Route render={()=>(
-					<NoMatch />
-				)} />
-			
 			</Switch>
 		  </div>
 		</BrowserRouter>
